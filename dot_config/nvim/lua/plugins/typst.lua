1070ceff99fb3c01c1a31035025525cc27fe53fa
scientiac <iacsci@proton.me>
2025-06-14 21:59:23 +0545

    nvim: typst automatically launch preview

--- dot_config/nvim/lua/plugins/typst.lua
+++ dot_config/nvim/lua/plugins/typst.lua
@@ -8,4 +8,15 @@ return {
     port = 31415,
     open_cmd = 'qutebrowser %s'
   },
+  config = function(_, opts)
+    require('typst-preview').setup(opts)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "typst",
+      callback = function()
+        vim.defer_fn(function()
+          vim.cmd("TypstPreview")
+        end, 100)
+      end,
+    })
+  end,
 }
