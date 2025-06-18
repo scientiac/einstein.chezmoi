d29e32dff1f79fc97bb3c16381c5f82cb518dc00
scientiac <iacsci@proton.me>
2025-06-18 20:15:27 +0545

    zsh: remove git aliases

--- dot_config/zsh/config.zsh
+++ dot_config/zsh/config.zsh
@@ -187,16 +187,7 @@ alias nvim='command nvim'
 alias vim='command nvim'
 alias vi='command nvim'
 
-# Git
-alias g='git'
-alias ga='git add'
-alias gc='git commit'
-alias gp='git push'
-alias gl='git pull'
-alias gs='git status'
-alias gd='git diff'
-alias gco='git checkout'
-alias gb='git branch'
+# git
 alias lg='lazygit'
 
 # System
