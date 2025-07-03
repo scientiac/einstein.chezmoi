d6c0ec9a08537bb474dd5c5db55213d5a323fb2e
scientiac <iacsci@proton.me>
2025-07-03 17:33:15 +0545

    add: wl-clipboard and battery script but it isnt working on way-edges

---
+++ dot_config/way-edges/executable_battery.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+battery_percentage() {
+  local battery=$(cat /sys/class/power_supply/BAT0/capacity 2>/dev/null)
+  if [ -z "$battery" ]; then
+    echo -n "0"
+  else
+    echo "scale=2; $battery / 100" | bc
+  fi
+}
+
+a=$(battery_percentage)
+
+# trim blank spaces and break lines
+# a=$(echo "$a" | tr -d '[:space:]' | tr -d '\n')
+
+echo -n "$a"
