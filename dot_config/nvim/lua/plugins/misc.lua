e6114d476d2dde80fa2ca27ee404cba5c1d308c0
scientiac <iacsci@proton.me>
2025-12-29 10:54:36 +0545

    feat!(nvim): use markview for markdown rendering

--- dot_config/nvim/lua/plugins/misc.lua
+++ dot_config/nvim/lua/plugins/misc.lua
@@ -1,10 +1,13 @@
 local conf_path = vim.fn.stdpath "config" --[[@as string]]
 return {
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    ft = "markdown",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    opts = {
+      preview = {
+        icon_provider = "mini", -- "mini" or "devicons"
+      }
+
     },
   },
 
@@ -14,14 +17,10 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     keys = {
       require("mappings").map({ "n" }, "<leader>p", function()
-        require("dropbar.api").pick(vim.v.count ~= 0 and vim.v.count)
+        require("dropbar.api").pick(vim.v.count ~= 0 and vim.v.count or nil)
       end, "Toggle dropbar menu"),
     },
     opts = {},
-  },
-
-  {
-    "farmergreg/vim-lastplace" 
   },
 
   {
@@ -34,8 +33,9 @@ return {
       "BufNewFile",
     },
     config = function()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = { "lua", "vimdoc", "rust", "go", "json", "toml", "markdown", "typst" },
+      ---@diagnostic disable-next-line: missing-fields
+      require("nvim-treesitter.config").setup {
+        ensure_installed = { "lua", "vimdoc", "rust", "go", "astro", "json", "toml", "markdown" },
         highlight = {
           enable = true,
           use_languagetree = true,
