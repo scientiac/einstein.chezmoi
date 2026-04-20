ca0ffa523e34bfa77544cdd86fd64e3e38406c52
scientiac <iacsci@proton.me>
2026-04-20 18:19:23 +0545

    chore!(nvim): smarana back and forward links

--- dot_config/nvim/lua/plugins/typst.lua
+++ dot_config/nvim/lua/plugins/typst.lua
@@ -37,7 +37,8 @@ if ok_sma then
   map("n", "<leader>sn", ":SmaNew<cr>", "New smarana note")
   map("n", "<leader>ss", ":SmaNotes<cr>", "Search smarana notes")
   map("n", "<leader>sk", ":SmaInsertLink<cr>", "Insert smarana link")
-  map("n", "<leader>sS", ":SmaSync<cr>", "Sync smarana")
+  map("n", "<leader>sb", ":SmaBacklinks<cr>", "View backlinks")
+  map("n", "<leader>sl", ":SmaForwardLinks<cr>", "View links")
 
   -- Direct type shortcuts (skip the type picker)
   map("n", "<leader>sf", ":SmaNewFleeting<cr>", "New fleeting note")
@@ -47,5 +48,6 @@ if ok_sma then
   -- Visual mode maps
   map("v", "<leader>st", smarana.new_from_title, "New note from title")
   map("v", "<leader>sc", smarana.new_from_content, "New note from content")
-  map("v", "<leader>sl", smarana.insert_link_at_selection, "Link selection")
+  map("v", "<leader>si", smarana.insert_link_at_selection, "Insert Link at selection")
+
 end
